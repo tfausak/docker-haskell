118f2ed69f9af6cc5e58c06849d37ba53d71aa15
Taylor Fausak <taylor@fausak.me>
2024-10-06 10:48:39 -0500

    Switch to Alpine

--- run/create-user.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env sh
-set -o errexit -o xtrace
-
-useradd --create-home --groups sudo --shell "$( command -v bash )" "$USER_NAME"
-echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" > "/etc/sudoers.d/$USER_NAME"
